dccc0b57beae0d0643fca9411113ca1045135b71
samiashajrawi <samiashajrawi@gmail.com>
2024-06-29 09:39:49 -0400

    Add Docker

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 const User = require('../models/userModel');
+const mongoUrl = process.env.MONGO_URL || 'mongodb://root:MjkwMTgtc2FtaWFz@localhost:27017';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://root:MjkwMTgtc2FtaWFz@localhost:27017');
+    await mongoose.connect(mongoUrl);
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection error:', error);
